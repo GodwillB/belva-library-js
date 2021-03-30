0dcae9cc423dafd3597911e0e5171a3851f34d92
Godwill Barasa <iamgodwillb@gmail.com>
2021-03-30 23:04:11 +0300

    MongoDB config

--- server/index.js
+++ server/index.js
@@ -6,3 +6,8 @@ import cors from 'cors';
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+//Mongodb Atlas Connection
+const CONNECTION_URL = 'mongodb+srv://belva-digital-library:belva-digital-library@belva-digital-library.3ybao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+';
